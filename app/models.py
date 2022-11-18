c4951ddc6ec92388ecf491eca39f9f19b5c3bee6
Gizar <gizarir@gmail.com>
2022-11-18 17:58:41 +0200

    App: added MailingsView

--- app/models.py
+++ app/models.py
@@ -1,10 +1,19 @@
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
+from django.urls import reverse
+
 
 class Mailing(models.Model):
     name = models.CharField(max_length=255, verbose_name="Имя рассылки")
     email_template = RichTextUploadingField(blank=True, default='', verbose_name="Шаблон")
     emails = models.TextField(blank=True, verbose_name="Список рассылки")
+
+    def __str__(self):
+        return f"{self.pk}, {self.name}, {self.email_template}, {self.emails}"
+
+    def get_absolute_url(self):
+        # home  - name of route
+        return reverse('home', kwargs={'pk': self.pk})
 
     class Meta:
         verbose_name = 'Рассылка'
--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
+
+from .models import *
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
@@ -11,3 +14,13 @@ def index(request):
         'app/index.html',
         {'my_text': 'Hello minimal configuration Django project', 'title': 'Name of tip'}
     )
+
+class MailingsView(ListView):
+    model = Mailing
+    template_name = 'app/list_mailings.html'
+    context_object_name = 'mailings'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title']='Список рассылок'
+        return context
